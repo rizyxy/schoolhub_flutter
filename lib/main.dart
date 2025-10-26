a479a780c10f5d292a0e0192fdf8d250ec41ed00
Mukhammad Rizki <rizkyRE7@gmail.com>
2025-10-26 15:10:31 +0700

    Load .env on startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main(List<String> args) {
+void main(List<String> args) async {
+  await dotenv.load(fileName: '.env');
+
   runApp(SchoolHub());
 }
 
