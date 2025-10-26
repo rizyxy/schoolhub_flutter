b024f0cd0d4286c2d5e0c62af1ab35c64d289cbc
Mukhammad Rizki <rizkyRE7@gmail.com>
2025-10-26 14:56:44 +0700

    Added Entry Point

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,16 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
+void main(List<String> args) {
+  runApp(SchoolHub());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class SchoolHub extends StatelessWidget {
+  const SchoolHub({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      home: Scaffold(),
     );
   }
 }
